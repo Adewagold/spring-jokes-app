891faf4c6e7fffb9456f2cdd8865d718755911cc
Adewale Adeleye <adewale.adeleye@interswitchgroup.com>
2019-03-26 07:44:46 +0000

    Ïnitial project commit

---
+++ src/main/java/ade/spring_joke_app/demo/SpringJokeAppApplication.java
@@ -0,0 +1,13 @@
+package ade.spring_joke_app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ade/spring_joke_app/demo/SpringJokeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package ade.spring_joke_app.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJokeAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
